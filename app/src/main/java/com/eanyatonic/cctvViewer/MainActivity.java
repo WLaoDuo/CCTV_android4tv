904a4bce909d2645203ac28033f6081e4f96e9b0
WLaoDuo <69497874+WLaoDuo@users.noreply.github.com>
2025-04-14 14:31:25 +0800

    cctvChannels,localChannels修改为全局变量

--- app/src/main/java/com/eanyatonic/cctvViewer/MainActivity.java
+++ app/src/main/java/com/eanyatonic/cctvViewer/MainActivity.java
@@ -86,11 +86,26 @@ public class MainActivity extends AppCompatActivity {
     private final String[] channelNames = { "1 CCTV-1 综合", "2 CCTV-2 财经", "3 CCTV-3 综艺", "4 CCTV-4 中文国际（亚）",
             "5 CCTV-5 体育", "6 CCTV-6 电影", "7 CCTV-7 国防军事", "8 CCTV-8 电视剧", "9 CCTV-9 纪录", "10 CCTV-10 科教",
             "11 CCTV-11 戏曲", "12 CCTV-12 社会与法", "13 CCTV-13 新闻", "14 CCTV-14 少儿", "15 CCTV-15 音乐", "16 CCTV-16 奥林匹克",
-            "17 CCTV-17 农业农村", "18 CCTV-5+ 体育赛事", "19 CCTV-4 中文国际（欧）", "20 CCTV-4 中文国际（美）", "21 北京卫视", "22 江苏卫视",
-            "23 东方卫视", "24 浙江卫视", "25 湖南卫视", "26 湖北卫视", "27 广东卫视", "28 广西卫视", "29 黑龙江卫视", "30 海南卫视", "31 重庆卫视",
-            "32 深圳卫视", "33 四川卫视", "34 河南卫视", "35 福建东南卫视", "36 贵州卫视", "37 江西卫视", "38 辽宁卫视", "39 安徽卫视", "40 河北卫视",
-            "41 山东卫视","42 央视频CCTV1", };
-
+            "17 CCTV-17 农业农村", "18 CCTV-5+ 体育赛事", "19 CCTV-4 中文国际（欧）", "20 CCTV-4 中文国际（美）",
+            
+            "21 北京卫视", "22 江苏卫视","23 东方卫视", "24 浙江卫视", "25 湖南卫视", "26 湖北卫视", 
+            "27 广东卫视", "28 广西卫视", "29 黑龙江卫视", "30 海南卫视", "31 重庆卫视","32 深圳卫视", 
+            "33 四川卫视", "34 河南卫视", "35 福建东南卫视", "36 贵州卫视", "37 江西卫视", "38 辽宁卫视",
+            "39 安徽卫视", "40 河北卫视","41 山东卫视","42 央视频CCTV1", 
+            };
+    private final String[] cctvChannels = {
+            "CCTV-1 综合", "CCTV-2 财经", "CCTV-3 综艺", "CCTV-4 中文国际（亚）",
+            "CCTV-5 体育", "CCTV-6 电影", "CCTV-7 国防军事", "CCTV-8 电视剧",
+            "CCTV-9 纪录", "CCTV-10 科教", "CCTV-11 戏曲", "CCTV-12 社会与法",
+            "CCTV-13 新闻", "CCTV-14 少儿", "CCTV-15 音乐", "CCTV-16 奥林匹克",
+            "CCTV-17 农业农村", "CCTV-5+ 体育赛事", "CCTV-4 中文国际（欧）", "CCTV-4 中文国际（美）",
+            };
+    private final String[] localChannels = {
+            "北京卫视", "江苏卫视", "东方卫视", "浙江卫视", "湖南卫视", "湖北卫视",
+            "广东卫视", "广西卫视", "黑龙江卫视", "海南卫视", "重庆卫视", "深圳卫视",
+            "四川卫视", "河南卫视", "福建东南卫视", "贵州卫视", "江西卫视", "辽宁卫视",
+            "安徽卫视", "河北卫视", "山东卫视", "央视频CCTV1",
+            };
     private int currentLiveIndex;
 
     private static final String PREF_NAME = "MyPreferences";
@@ -251,13 +266,13 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout subMenuLocal = findViewById(R.id.subMenuLocal);
 
         // 中央台频道列表
-        String[] cctvChannels = {
-                "CCTV-1 综合", "CCTV-2 财经", "CCTV-3 综艺", "CCTV-4 中文国际（亚）",
-                "CCTV-5 体育", "CCTV-6 电影", "CCTV-7 国防军事", "CCTV-8 电视剧",
-                "CCTV-9 纪录", "CCTV-10 科教", "CCTV-11 戏曲", "CCTV-12 社会与法",
-                "CCTV-13 新闻", "CCTV-14 少儿", "CCTV-15 音乐", "CCTV-16 奥林匹克",
-                "CCTV-17 农业农村", "CCTV-5+ 体育赛事", "CCTV-4 中文国际（欧）", "CCTV-4 中文国际（美）"
-        };
+        // String[] cctvChannels = {
+        //         "CCTV-1 综合", "CCTV-2 财经", "CCTV-3 综艺", "CCTV-4 中文国际（亚）",
+        //         "CCTV-5 体育", "CCTV-6 电影", "CCTV-7 国防军事", "CCTV-8 电视剧",
+        //         "CCTV-9 纪录", "CCTV-10 科教", "CCTV-11 戏曲", "CCTV-12 社会与法",
+        //         "CCTV-13 新闻", "CCTV-14 少儿", "CCTV-15 音乐", "CCTV-16 奥林匹克",
+        //         "CCTV-17 农业农村", "CCTV-5+ 体育赛事", "CCTV-4 中文国际（欧）", "CCTV-4 中文国际（美）"
+        // };
 
         // 动态生成中央台按钮
         for (String channel : cctvChannels) {
@@ -277,12 +292,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // 地方台频道列表
-        String[] localChannels = {
-                "北京卫视", "江苏卫视", "东方卫视", "浙江卫视", "湖南卫视", "湖北卫视",
-                "广东卫视", "广西卫视", "黑龙江卫视", "海南卫视", "重庆卫视", "深圳卫视",
-                "四川卫视", "河南卫视", "福建东南卫视", "贵州卫视", "江西卫视", "辽宁卫视",
-                "安徽卫视", "河北卫视", "山东卫视", "央视频CCTV1"
-        };
+        // String[] localChannels = {
+        //         "北京卫视", "江苏卫视", "东方卫视", "浙江卫视", "湖南卫视", "湖北卫视",
+        //         "广东卫视", "广西卫视", "黑龙江卫视", "海南卫视", "重庆卫视", "深圳卫视",
+        //         "四川卫视", "河南卫视", "福建东南卫视", "贵州卫视", "江西卫视", "辽宁卫视",
+        //         "安徽卫视", "河北卫视", "山东卫视", "央视频CCTV1"
+        // };
 
         // 动态生成地方台按钮
         for (String channel : localChannels) {
